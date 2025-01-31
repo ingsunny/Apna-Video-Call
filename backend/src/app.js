5c17278c7717889e80201e6516171e2adac9fa41
Sunny Patel <sunnypatel.koder@gmail.com>
2025-01-31 20:39:28 +0530

    auth: error resolved

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,8 +13,7 @@ const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
-
-app.set("port", (process.env.PORT || 8000))
+app.set("port", process.env.PORT || 8000);
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
@@ -22,19 +21,15 @@ app.use(express.urlencoded({ limit: "40kb", extended: true }));
 app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
-    app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://videocall:videocall100@cluster0.e0niu.mongodb.net/")
+  app.set("mongo_user");
+  const connectionDb = await mongoose.connect(
+    "mongodb+srv://videocall:videocall100@cluster0.e0niu.mongodb.net/"
+  );
 
-
-    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
-    server.listen(app.get("port"), () => {
-        console.log("LISTENIN ON PORT 8000")
-    });
-
-
-
-}
-
-
+  console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`);
+  server.listen(app.get("port"), () => {
+    console.log("LISTENIN ON PORT 8000");
+  });
+};
 
 start();
